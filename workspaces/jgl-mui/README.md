e2a79bb7164e84feefd6defec3d32d8fd2cd899c
jagel <jagel8@hotmail.com>
2025-10-29 18:54:01 -0400

    Library configuration

---
+++ workspaces/jgl-mui/README.md
@@ -0,0 +1,9 @@
+# MUI
+
+Revision: 10/27/2025
+
+Packages required
+
+``` bash 
+npm install @mui/material @emotion/react @emotion/styled
+```
---
+++ workspaces/jgl-mui/src/index.ts
@@ -0,0 +1,10 @@
+// Main entry point for the jgl-mui library
+// Export your components here
+
+// Example:
+// export { default as JglTable } from './components/JglTable';
+// export type { JglTableProps } from './components/JglTable';
+
+// TODO: Add your actual component exports here
+
+export * from './table/jgl-table-header';
