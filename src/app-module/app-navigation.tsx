db8f45b27cbb06df49f9ea5adc68d0fa256ecfd4
jagel <jagel8@homtail.com>
2025-11-02 21:01:26 -0500

    Navigation chagnes

--- src/app-module/app-navigation.tsx
+++ src/app-module/app-navigation.tsx
@@ -1,21 +1,24 @@
 // #region Imports
 // React and external libraries
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useMemo, useState } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 // MUI Components and Icons
-import { 
-    Drawer, 
-    List, 
-    ListItem, 
-    ListItemIcon, 
-    ListItemText, 
-    IconButton,
-    useTheme
-} from '@mui/material';
-import Icon from '@mdi/react';
-import { mdiChartBar, mdiChevronLeft, mdiChevronRight, mdiHome } from '@mdi/js';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import IconButton from '@mui/material/IconButton';
+import { useTheme } from '@mui/material';
 
+import Icon from '@mdi/react';
+import { mdiChevronLeft, mdiChevronRight, mdiLock, mdiLockOpen } from '@mdi/js';
+
+// JGL Library
+import MenuListItem from '@jgl-mui/components/menu-list-item';
+
+// Internal imports
+import { AppRoutes, authRoutes } from '../app-setup/app-routing';
 
 // Styles
 import './app-navigation.css';
@@ -26,23 +29,32 @@ interface AppNavigationProps {
     open: boolean;
     onClose: () => void;
 }
+
 // #endregion Interfaces
 
 export const AppNavigation: React.FC<AppNavigationProps> = ({ open, onClose }) => {
     // #region Initializations
     const theme = useTheme();
     const navigate = useNavigate();
+    const location = useLocation();
 
-    const menuItems = [
-        { text: 'Home', icon: <Icon path={mdiHome} size={1} />, path: '/' },
-        { text: 'Table Example', icon: <Icon path={mdiChartBar} size={1} /> , path: '/table-example' }
-    ];
+    const menuItems = useMemo(() : AppRoutes[] => (Object.values(authRoutes) ), []);
+    const [lockDrawer, setLockDrawer] = useState(false);
+
     // #endregion Initializations
 
     // #region Events
     const handleNavigate = (path: string): void => {
         navigate(path);
-        onClose();
+        if(!lockDrawer){
+            onClose();
+        }
+    };
+
+    const onCloseBtnClick = () => {
+        if(!lockDrawer){
+            onClose();
+        }
     };
     // #endregion Events
 
@@ -54,27 +66,30 @@ export const AppNavigation: React.FC<AppNavigationProps> = ({ open, onClose }) =
             open={open}
             className="app-navigation"
         >
-            <div className="drawer-header">
-                <IconButton onClick={onClose}>
-                    {theme.direction === 'ltr' ? 
-                        <Icon path={mdiChevronLeft} size={1.2} /> : 
-                        <Icon path={mdiChevronRight} size={1.2} />}
-                </IconButton>
-            </div>
-            <List>
+            <List component={"nav"}>
                 {menuItems.map((item, index) => (
-                    <ListItem 
-                        component="div"
+                    <MenuListItem
                         key={index}
                         onClick={() => handleNavigate(item.path)}
-                        className="navigation-item"
+                        className={'navigation-item-' + index}
                         sx={{ cursor: 'pointer' }}
+                        isActive={location.pathname === item.path} // Add this line
                     >
                         <ListItemIcon>{item.icon}</ListItemIcon>
-                        <ListItemText primary={item.text} />
-                    </ListItem>
+                        <ListItemText primary={item.name} />
+                    </MenuListItem>
                 ))}
             </List>
+
+            <div className="drawer-header">
+                 <IconButton color='secondary' onClick={() => setLockDrawer(prev => !prev)}>
+                    { <Icon path={lockDrawer ? mdiLock : mdiLockOpen} size={1} /> } 
+                </IconButton>
+
+                <IconButton disabled={lockDrawer} color='primary' onClick={(onCloseBtnClick)}>
+                    { <Icon path={ theme.direction === 'ltr' ? mdiChevronLeft : mdiChevronRight} size={1} /> }
+                </IconButton>
+            </div>
         </Drawer>
     );
     // #endregion Render
