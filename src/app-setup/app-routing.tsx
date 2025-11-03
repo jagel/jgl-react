070d0e1a20a9080ceace50d41da5ee39a50a5709
jagel <jagel8@homtail.com>
2025-11-02 21:00:34 -0500

    Routing chagnes

--- src/app-setup/app-routing.tsx
+++ src/app-setup/app-routing.tsx
@@ -1,16 +1,21 @@
 // #region Imports
 // React and external libraries
+import { mdiButtonCursor, mdiChartBar, mdiHome } from "@mdi/js";
+import Icon from "@mdi/react";
 import React from "react";
 // #endregion Imports
 
 
-interface AppRoutes {
+export interface AppRoutes {
     name: React.ReactNode;
     path: string;
+    hideInNavigation?:boolean;
+    icon?: React.ReactNode;
 }
 
 /**  Routes definitions for breadcrumbs creation */
-export const authRoutes = {
-    root: { name:'Home', path:'/' } as AppRoutes,
-    tablePage: { name:'Table', path:'/table-example' } as AppRoutes,
+export const authRoutes : Record<string, AppRoutes> = {
+    root: { name:'Home', path:'/', icon: <Icon path={mdiHome} size={1} /> } as AppRoutes,
+    tablePage: { name:'Table', path:'/table-example', icon: <Icon path={mdiChartBar} size={1} /> } as AppRoutes,
+    buttonsShowcase: { name:'Buttons Showcase', path:'/buttons-showcase', icon: <Icon path={mdiButtonCursor} size={1} /> } as AppRoutes,
 }
