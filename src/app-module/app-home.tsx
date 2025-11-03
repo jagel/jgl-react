9500bf59489f0212bbdc4270403e122d3260e184
jagel <jagel8@homtail.com>
2025-11-02 21:02:53 -0500

    Dartk theme and standard styles changes

--- src/app-module/app-home.tsx
+++ src/app-module/app-home.tsx
@@ -1,10 +1,27 @@
 // #region Imports
 // React and external libraries
-import React from "react";
+import { Button } from "@mui/material";
+import { useColorScheme } from "@mui/material/styles";
+import React, { useState } from "react";
 // #endregion Imports
 
 const AppHome: React.FC = () => {
+        const { setMode } = useColorScheme();
+const [islight, setislight] = useState(true);
+
+    const toggleMode = () => {
+        if (islight) {
+            setMode('dark');
+        } else {
+            setMode('light');
+        }
+        setislight(!islight);
+    }
+    
     return <>
+    <Button variant="contained" onClick={toggleMode} sx={{ mb: 2 }}>
+        {islight ? 'Light Mode ➡️ Dark Mode' : 'Dark Mode ➡️ Light Mode'}
+    </Button>
         <h1>Welcome to the Application Home Page</h1>
         <p>This is the main landing page of the application.</p>
     </>
--- src/app-module/app-layout.tsx
+++ src/app-module/app-layout.tsx
@@ -3,11 +3,16 @@
 import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
 
+// MUI
+import Button from "@mui/material/Button";
+import Icon from "@mdi/react";
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import { mdiMenuClose, mdiMenuOpen } from "@mdi/js";
+
 // Internal modules and components
 import AppNavigation from "./app-navigation";
-import Button  from "@mui/material/Button";
-import Icon from "@mdi/react";
-import { mdiMenuClose, mdiMenuOpen } from "@mdi/js";
 // #endregion Imports
 
 
@@ -15,22 +20,30 @@ const AppLayout: React.FC = () => {
     // #region Initializations
     const [drawerOpen, setDrawerOpen] = useState(false);
     // #endregion Initializations
-
-    return <> 
-     <AppNavigation open={drawerOpen} onClose={()=>setDrawerOpen(false)} />
-    <div style={{display:'flex', flexDirection:'column', height:'100vh', gap:'10px'}}>
-        <nav style={{paddingLeft:drawerOpen? '250px':'10px', transition:'padding-left 0.2s'}}>
-            <Button variant="outlined" onClick={() => setDrawerOpen(prev => !prev)}>
-                <Icon path={drawerOpen ? mdiMenuOpen : mdiMenuClose} size={1} />
-            </Button>
-        </nav>
-        <div style={{paddingLeft:drawerOpen? '250px':'10px', transition:'padding-left 0.2s'}}>
-            {/* This is where nested route components will be rendered */}
-            <React.Suspense fallback={<div>Loading...</div>}>
-                <Outlet />
-            </React.Suspense>
-        </div>
-    </div>
+    const sxPaddingAdjustment = {
+        paddingLeft: drawerOpen ? '250px' : '10px',
+        paddingRight: '10px',
+        transition: 'padding-left 0.2s'
+    };
+    return <>
+    <Box>
+        <AppBar position="sticky" sx={sxPaddingAdjustment}>
+            <Toolbar>
+                <Button color="inherit" onClick={() => setDrawerOpen(prev => !prev)}>
+                    <Icon path={drawerOpen ? mdiMenuOpen : mdiMenuClose} size={1} />
+                </Button>
+            </Toolbar>
+        </AppBar>
+        <Box sx={{paddingTop: '16px'}}>
+            <AppNavigation open={drawerOpen} onClose={() => setDrawerOpen(false)} />
+                
+            <Box sx={sxPaddingAdjustment}>
+                <React.Suspense fallback={<div>Loading...</div>}>
+                    <Outlet />
+                </React.Suspense>
+            </Box>
+        </Box>
+    </Box>
     </>
 }
 
