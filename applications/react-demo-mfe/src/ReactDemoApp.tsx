9badd302fd9648192d1dd83408eab3c4927329f5
jagel <jagel8@homtail.com>
2026-01-29 20:21:09 -0500

    routes added

--- applications/react-demo-mfe/src/ReactDemoApp.tsx
+++ applications/react-demo-mfe/src/ReactDemoApp.tsx
@@ -1,17 +1,67 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { ThemeProvider } from '@mui/material/styles';
+import  Paper  from '@mui/material/Paper';
 import CssBaseline from '@mui/material/CssBaseline';
 import { theme } from './styles/theme';
 import DemoLayout from './components/DemoLayout';
 import ButtonsShowcase from './buttons/buttons-showcase';
+import { createBrowserRouter, RouteObject, Outlet, RouterProvider } from 'react-router-dom';
+import Box from '@mui/material/Box';
+
+const TestComponent1 = () => {
+  return <div>Test Component 1</div>;
+};
+
+const TestComponent2 = () => {
+  return <div>Test Component 2</div>;
+};
+
+const AppLayout: React.FC = () => {
+ 
+  return <>
+  <Box>
+    hellooo
+    <Outlet />
+  </Box>
+  </>
+}
+
 
 const ReactDemoApp: React.FC = () => {
+const isAuthenticated = true;
+  const routes = useMemo((): RouteObject[] => (
+    !isAuthenticated ?
+        // Unauthenticated routes
+        [{
+            path: "*",
+            element: <ButtonsShowcase />,
+        }] :
+        // Authenticated routes
+        [{
+            path: "react-demo/",
+            element: <AppLayout />,
+
+            children: [{
+                index: true,
+                element: <TestComponent1 />
+            }, {
+                path: 'test2',
+                element: <TestComponent2 />
+            }]
+        }])
+    , [isAuthenticated]); // Recompute routes only when authentication status changes
+
+// Memoized router instance based on the routes array
+const router = useMemo(() => createBrowserRouter(routes), [routes]);
+
   return (
     <ThemeProvider theme={theme}>
+      <Paper>
       <CssBaseline />
       <DemoLayout>
-        <ButtonsShowcase />
+      <RouterProvider router={router} />
       </DemoLayout>
+      </Paper>
     </ThemeProvider>
   );
 };
